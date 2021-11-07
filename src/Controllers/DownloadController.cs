597a57d34e121f3f248e39249af1dec2a5a62c9f
九零 <78294929+JiuLing-zhang@users.noreply.github.com>
2021-11-07 20:49:02 +0800

    新增
    1、下载时简单记录个日志

--- src/Controllers/DownloadController.cs
+++ src/Controllers/DownloadController.cs
@@ -1,6 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
 using System.IO;
 using System.Web;
+using EasyLogRepository.DbContext;
+using EasyLogRepository.Models;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 namespace EasyLogRepository.Controllers
@@ -9,16 +13,27 @@ namespace EasyLogRepository.Controllers
     [ApiController]
     public class DownloadController : Controller
     {
-
+        private readonly MyDbContext _dbContext;
         private readonly IHostEnvironment _hostEnvironment;
-        public DownloadController(IHostEnvironment hostEnvironment)
+        public DownloadController(IHostEnvironment hostEnvironment, IServiceScopeFactory factory)
         {
             _hostEnvironment = hostEnvironment;
+            _dbContext = factory.CreateScope().ServiceProvider.GetRequiredService<MyDbContext>();
         }
 
         [HttpGet("{path}")]
         public IActionResult Get(string path)
         {
+            var log = new TableLogs()
+            {
+                CreateTime = DateTime.Now,
+                LogType = LogTypeEnum.消息,
+                SessionId = "Download",
+                Message = "文件下载"
+            };
+            _dbContext.Logs.Add(log);
+            _dbContext.SaveChanges();
+
             var file = HttpUtility.UrlDecode(path);
             file = file.Replace("/", "\\");
             string filePath = Path.Combine(_hostEnvironment.ContentRootPath, file);
