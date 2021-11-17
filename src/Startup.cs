e85dd59b8e1b1b5447b0765a1a07876c5d8c6a27
九零 <78294929+JiuLing-zhang@users.noreply.github.com>
2021-11-17 23:18:58 +0800

    优化
    1、删除不必要的Using

--- src/Startup.cs
+++ src/Startup.cs
@@ -1,17 +1,11 @@
 using EasyLogRepository.Data;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using EasyLogRepository.DbContext;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.StaticFiles;
 using Microsoft.EntityFrameworkCore;
 
